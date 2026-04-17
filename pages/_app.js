ccfa2897bbc171c9d44e950a0313ac1d6e46caf5
CHiiRAG-09 <gogoaheadgo@gmail.com>
2026-04-18 00:36:14 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,13 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📈</text></svg>" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
